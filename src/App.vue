704d7d2334bc85e8ebf80089f29ee7f1fe6649bb
shellRaining <shellRaining@gmail.com>
2024-08-13 22:01:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import WaveSurfer from "wavesurfer.js";
+import Spectrogram from "wavesurfer.js/dist/plugins/spectrogram";
+
+import { onMounted, ref } from "vue";
+
+onMounted(() => {});
+</script>
+
+<template>
+  <div>
+    <div id="wavesurfer"></div>
+  </div>
+</template>
+
+<style scoped></style>
